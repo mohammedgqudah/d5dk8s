8337b4e7b2248712f9f73937c58135114a235ed8
mqudah <mohghq@gmail.com>
2024-06-10 01:28:01 +0300

    wip

--- d5dk8s/cogs/pods.py
+++ d5dk8s/cogs/pods.py
@@ -2,42 +2,23 @@ import logging
 from datetime import datetime, timedelta
 import discord
 from discord.ext import commands, tasks
-from sqlalchemy import Row, insert, update
+from sqlalchemy import insert, update
+from sqlalchemy.ext.asyncio.engine import AsyncConnection
 from d5dk8s.kubernetes import get_pods
 from d5dk8s.database.database import engine
 from d5dk8s.database.tables import ResourceType, watchers
 from d5dk8s.config import Config
-from d5dk8s.utils.helpers import chunk
+from d5dk8s.utils.messages import get_message
+from d5dk8s.utils.pods import pods_to_embeds
 
 logging.basicConfig(level=logging.INFO)
 logging.root.setLevel(logging.DEBUG)
 
 
-async def list_watchers():
-    async with engine.connect() as conn:
-        result = await conn.execute(watchers.select())
+async def list_watchers(connection: AsyncConnection):
+    """List all watchers"""
+    result = await connection.execute(watchers.select())
     return result.fetchall()
-
-def pods_to_embed(pods):
-    """Create an embed containing a list of pods."""
-    embed = discord.Embed(title="Pods", color=0x447ceb)
-    
-    for pod in pods:
-        pod_name = pod['metadata']['name']
-        pod_status = pod['status']['phase']
-        pod_status = pod_status if pod_status == 'Running' else f"~~{pod_status}~~"
-        embed.add_field(name=pod_name, value=pod_status, inline=True)
-    return embed
-
-
-def pods_to_embeds(pods):
-    """Return a list of embeds containing all pods"""
-    embeds = []
-    # a single embed can only contain 25 fields
-    for pods_chunk in chunk(pods, 25):
-        embeds.append(pods_to_embed(pods_chunk))
-
-    return embeds
 
 class Pods(commands.Cog):
     bot: discord.Bot
@@ -58,6 +39,7 @@ class Pods(commands.Cog):
     async def watch(self, ctx: discord.ApplicationContext, namespace: str, refresh_interval: discord.Option(int, description="In minutes")):
         """Register a new watcher for pods."""
         pods = await get_pods(namespace)
+
         # the interaction response cannot be edited, as a workaround, the interaction
         # will be acknowledged and another message will be sent containing a list of pods
         await ctx.respond(":white_check_mark: Watcher added.", ephemeral=True)
@@ -77,25 +59,21 @@ class Pods(commands.Cog):
             await conn.execute(stmt)
             await conn.commit()
 
-    @tasks.loop(seconds=10)
+    @tasks.loop(minutes=1)
     async def update_watchers(self):
-        result = await list_watchers()
-        logging.info(f"found {len(result)} watchers")
-
         async with engine.connect() as conn:
+            result = await list_watchers(conn)
+            logging.info(f"found {len(result)} watchers")
+
             for watcher in result:
                 time_since_last_refresh: timedelta = (datetime.now() - watcher.last_refresh_time)
                 if time_since_last_refresh.seconds > watcher.refresh_interval_seconds:
                     logging.info(f"updating watcher #{watcher.id} for namespace {watcher.namespace}")
-
-                    guild = await self.bot.fetch_guild(watcher.guild_id, with_counts=False)
-                    channel = await guild.fetch_channel(watcher.channel_id)
-                    message = await channel.fetch_message(watcher.message_id)
+                    message = await get_message(self.bot, watcher.guild_id, watcher.channel_id, watcher.message_id)
 
                     pods = await get_pods(watcher.namespace)
                     await message.edit(embeds=pods_to_embeds(pods))
 
-                    # update the watcher
                     stmt = update(watchers).where(watchers.c.id == watcher.id).values(last_refresh_time=datetime.now())
                     await conn.execute(stmt)
                     await conn.commit()
@@ -103,3 +81,4 @@ class Pods(commands.Cog):
 
 def setup(bot):
     bot.add_cog(Pods(bot))
+
